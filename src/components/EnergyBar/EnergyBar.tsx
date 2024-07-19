b330a995cc95682f5c5d3c72b504797c69dc51b2
Ervin Khamoido <ervin.khamoido@gmail.com>
2024-07-19 14:37:34 +0300

    [se-124] Display energy amount

--- src/components/EnergyBar/EnergyBar.tsx
+++ src/components/EnergyBar/EnergyBar.tsx
@@ -3,14 +3,19 @@ import styles from './EnergyBar.module.css';
 import { useGameData } from '@/hooks';
 
 const EnergyBar: FC = () => {
-  const { energy } = useGameData();
+  const { energy, maxEnergy } = useGameData();
 
   const enegryAmount = Math.min(Math.ceil(energy / 100), 12);
   const componentsToRender = Array.from({ length: enegryAmount }, (_, index) => (
     <img key={index} src={'/assets/lightning.svg'} alt={'Lightning icon'} />
   ));
 
-  return <div className={styles.energyBar}>{componentsToRender}</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.energyBar}>{componentsToRender}</div>
+      <span className={styles.label}>{energy}/{maxEnergy}</span>
+    </div>
+  );
 };
 
 export default EnergyBar;
